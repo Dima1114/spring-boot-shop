e0de76d472d14106aa07a57384f5cec9d92b3da6
Dima1114 <Dimas1114@yandex.ru>
2018-01-27 21:51:23 +0200

    carousel changes

--- src/main/resources/static/js/profile-carousel.js
+++ src/main/resources/static/js/profile-carousel.js
@@ -3,42 +3,35 @@ var itemsInRow = $('#itemsInRow').text();
 //everything resolves in template by adding attribute data-ride
 $(document).ready(function () {
 
-    if ($('.carousel-item').length > itemsInRow) {
+    if ($('.small-carousel').length > itemsInRow) {
 
         //I don`t know why, but it does not work
         // $('.carousel').carousel({
         //     interval: 1000
         // });
-
-
-        $('.carousel .carousel-item').each(function () {
+        $('.carousel .small-carousel').each(function () {
             var itemToClone = $(this);
             for (var i = 0; i < itemsInRow; i++) {
                 itemToClone = itemToClone.next();
-
                 // wrap around if at end of item collection
                 if (!itemToClone.length) {
                     itemToClone = $(this).siblings(':first');
                 }
-
                 // grab item, clone, add marker class, add to collection
                 itemToClone.children(':first-child').clone()
                     .appendTo($(this));
             }
         });
-
     } else {
-
         //if items less than 4 then stop sliding
         $('.carousel').carousel('pause');//this row does not work too
 
-        var itemSize = $('.carousel .carousel-item').length;
-        $('.carousel .carousel-item').each(function () {
+        var itemSize = $('.carousel .small-carousel').length;
+        $('.carousel .small-carousel').each(function () {
 
             var itemToClone = $(this);
             for (var i = 0; i < itemSize; i++) {
                 itemToClone = itemToClone.next();
-
                 // grab item, clone, add marker class, add to collection
                 itemToClone.children(':first-child').clone()
                 // .addClass("cloneditem-"+(i))
@@ -46,12 +39,7 @@ $(document).ready(function () {
             }
         });
     }
-
 });
 
-function dubbleClick() {
-        alert('dbl');
-
-}
 
 
